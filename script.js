f1a2cc9b983445af432adb6378c9392015d9984d
Keyesprime <axios230221@gmail.com>
2026-01-28 01:55:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,25 +4,34 @@ const audioGroups = [
     title: "💬 很日常的日常用语",
     items: [
       { label: "Ciallo～(∠・ω< )⌒★", file: "audio/ciallo.mp3" },
+      { label: "呀吼", file: "audio/yahoo.mp3" },
+      { label: "嗯~", file: "audio/hum.mp3" },
       { label: "小蛮人真棒", file: "audio/小蛮人真棒.mp3" },
       { label: "真棒1", file: "audio/真棒1.mp3" },
       { label: "真棒爽朗版！", file: "audio/真棒爽朗版.mp3" },
       { label: "恼火🤯", file: "audio/恼火.mp3" },
       { label: "拜拜👋", file: "audio/拜拜.mp3" },
       { label: "好狗狗🦴", file: "audio/好狗狗.mp3" },
+      { label: "妈妈在哦", file: "audio/妈妈在哦.mp3" },
       { label: "怪叫", file: "audio/怪叫1.mp3" },
       { label: "真是淫乱呢", file: "audio/真是淫乱呢.mp3" },
       { label: "收钱音效", file: "audio/收钱音效.mp3" },
+      { label: "对的对的！", file: "audio/对的对的.mp3" },
       { label: "不对！", file: "audio/不对不对.mp3" },
     ]
   },
     {
     title: "🤭 别逗你白姐笑了",
     items: [
-      { label: "那你也蛮贱的", file: "audio/那你也蛮贱的.mp3" },
+      { label: "想被骂了？那你也蛮贱的", file: "audio/那你也蛮贱的.mp3" },
+      { label: "那你也蛮贱的", file: "audio/那你也蛮贱de.mp3" },
+      { label: "他们就是贱！", file: "audio/他们就是贱.mp3" },
+      { label: "大家就是贱！", file: "audio/大家就是贱.mp3" },
+      { label: "怎么这么没用", file: "audio/怎么这么没用.mp3" },
       { label: "人类还69呢", file: "audio/人类还69呢.mp3" },
       { label: "那男同还当搅屎棍呢", file: "audio/男同还当搅屎棍呢.mp3" },
       { label: "假清高", file: "audio/假清高.mp3" },
+      { label: "那咋了", file: "audio/那咋了.mp3" },
       { label: "主播是S", file: "audio/主播是S.mp3" },
       { label: "直播间不是只搞黄色直播间吗", file: "audio/只搞黄色直播间.mp3" },
       { label: "大家还是太性压抑了", file: "audio/太性压抑了.mp3" },
@@ -38,7 +47,8 @@ const audioGroups = [
       { label: "エロい~", file: "audio/eroi.mp3" },
       { label: "不知道", file: "audio/希腊奶.mp3" },
       { label: "真的？", file: "audio/真的？.mp3" },
-      { label: "达咩", file: "audio/达咩.mp3" }
+      { label: "达咩", file: "audio/达咩.mp3" },
+      { label: "kimo...", file: "audio/kimo.mp3" }
     ]
   }
 ];
